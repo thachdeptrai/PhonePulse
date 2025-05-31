32581bbfe7326791135f0f2f87c821ca7236f9fe
chientd1607 <162539116+chientd1607@users.noreply.github.com>
2025-05-31 14:44:29 +0700

    Thêm models

---
+++ User.java
@@ -0,0 +1,79 @@
+package com.phoneapp.phonepulse.data.network;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password_hash;
+    private String phone;
+    private String avatar_url;
+    private boolean is_admin;
+    private String created_at; // hoặc date
+
+    // getter vs setter
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword_hash() {
+        return password_hash;
+    }
+
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public boolean isIs_admin() {
+        return is_admin;
+    }
+
+    public void setIs_admin(boolean is_admin) {
+        this.is_admin = is_admin;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+}
